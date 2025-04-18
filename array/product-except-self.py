e51d61e642b889b57c37e06fdb51e69fd38b40f7
ycheryl <yulcheryl2357@gmail.com>
2025-04-18 15:22:15 +0700

    product of array except self

---
+++ array/product-except-self.py
@@ -0,0 +1,45 @@
+"""
+Given an integer array nums, return an array answer such that answer[i]
+is equal to the product of all the elements of nums except nums[i].
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+Solution: traverse more than 1
+"""
+
+
+# Time complexity: O(n)
+# Space complexity: O(n) for prefix and suffix arrays
+# We can reduce the space complexity to O(1) by using the output array
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        res = [1] * n
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+
+        for i in range(n):
+            res[i] = prefix[i] * suffix[i]
+
+        return res
+
+    def productExceptSelf2(self, nums):
+        n = len(nums)
+        res = [1] * n
+
+        for i in range(1, n):
+            res[i] = res[i - 1] * nums[i - 1]
+
+        prevSuffix = 1
+        for i in range(n - 2, -1, -1):
+            prevSuffix *= nums[i + 1]
+            res[i] *= prevSuffix
+
+        return res
