13d118d4bef19a87ee254be77c65750dcf476d4e
ycheryl <yulcheryl2357@gmail.com>
2025-04-24 08:39:30 +0700

    add group anagram

--- string/anagram.py
+++ string/anagram.py
@@ -1,13 +1,12 @@
-"""
-Given two strings s and t, return true if t is an anagram of s,
-and false otherwise.
-Solution: counter
-Time complexity: O(n)
-Space complexity: O(n)
-"""
-
-
 class Solution(object):
+    """
+    Given two strings s and t, return true if t is an anagram of s,
+    and false otherwise.
+    Solution: counter
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+
     def createCounter(self, s):
         counter = {}
         for char in s:
@@ -38,3 +37,33 @@ class Solution(object):
             counter_s[ord(s[i]) - ord("a")] += 1
             counter_t[ord(t[i]) - ord("a")] += 1
         return counter_s == counter_t
+
+    """
+    2. Given an array of strings strs, group the anagrams together.
+    Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+    Solution: hash table
+    Time complexity: O(n * k)
+    Space complexity: O(n * k)
+    where n is the length of strs and k is the maximum length of a string 
+    in strs.
+    """
+
+    def createKeys(self, string):
+        counter = [""] * 26
+        for char in string:
+            counter[ord(char) - ord("a")] += char
+        return "".join([el for el in counter])
+
+    def groupAnagrams(self, strs):
+        hash_table = {}
+        for string in strs:
+            hash_key = self.createKeys(string)
+            if hash_key in hash_table:
+                hash_table[hash_key].append(string)
+            else:
+                hash_table[hash_key] = [string]
+        res = []
+        for i in hash_table:
+            res.append(hash_table[i])
+        return res
