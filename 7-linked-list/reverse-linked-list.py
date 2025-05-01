3dc65b9e50e86b5d1a3d83e294a1366d686c2bbc
ycheryl <yulcheryl2357@gmail.com>
2025-05-01 13:26:21 +0700

    add list node

--- 7-linked-list/reverse-linked-list.py
+++ 7-linked-list/reverse-linked-list.py
@@ -5,6 +5,12 @@ Space Complexity: O(1)
 """
 
 
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution(object):
     def reverseList(self, head):
         prev = None
