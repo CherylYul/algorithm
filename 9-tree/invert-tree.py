28d89a8f64cbc9884c6a0222c5c1544a7ba57698
ycheryl <yulcheryl2357@gmail.com>
2025-05-09 17:23:26 +0700

    flip binary tree

---
+++ 9-tree/invert-tree.py
@@ -0,0 +1,16 @@
+"""
+LeetCode 226. Invert Binary Tree
+Given the root of a binary tree, invert the tree, and return its root.
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return root
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root
