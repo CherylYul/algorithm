62594075a9f260b2dd741f8074fb59915cf5ea22
ycheryl <yulcheryl2357@gmail.com>
2025-07-02 21:40:52 +0700

    add longest palindrome

--- 2-string/palindrome.py
+++ 2-string/palindrome.py
@@ -2,7 +2,16 @@
 A phrase is a palindrome if, after converting all uppercase letters into
 lowercase letters and removing all non-alphanumeric characters, it reads
 the same forward and backward.
+Time complexity: O(n)
+Space complexity: O(n)
+
+Longest Palindrome:
+Solution: counter
+Time complexity: O(n)
+Space complexity: O(n)
 """
+
+import re
 
 
 class Solution(object):
@@ -11,3 +20,22 @@ class Solution(object):
             return True
         s_strip = "".join(char.lower() for char in s if char.isalnum())
         return s_strip == s_strip[::-1]
+
+    # using regex to remove non-alphanumeric characters
+    def isPalindrome2(self, s: str) -> bool:
+        s = re.sub("[^a-zA-Z0-9]", "", s).lower()
+        return s == s[::-1]
+
+    def longestPalindrome(self, s):
+        if len(s) == 1:
+            return 1
+        s_dict = {}
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+        length = 0
+        for char in s_dict:
+            if s_dict[char] % 2 == 0:
+                length += s_dict[char]
+            else:
+                length += s_dict[char] - 1
+        return length + 1 if length < len(s) else length
