c64a545337ce8c3712591636be17053a5b0bc445
ycheryl <yulcheryl2357@gmail.com>
2025-07-02 10:36:31 +0700

    merge 2 sorted list

---
+++ 7-linked-list/merge-two-sorted-list.py
@@ -0,0 +1,30 @@
+"""
+Merge Two Sorted Lists
+Solution: dummy node and pointer
+Time Complexity: O(n + m), where n and m are the lengths of the two lists
+Space Complexity: O(1), we reuse existing nodes, no extra list or array is created.
+"""
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(-1)
+        current = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        current.next = list1 if list1 else list2
+        return dummy.next
