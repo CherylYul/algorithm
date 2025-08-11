1b3172da16c55f8ad39571b8471fb66ba5fa72ed
ycheryl <yulcheryl2357@gmail.com>
2025-08-11 23:02:20 +0700

    add prefix sum and monotonic stack approach

--- 1-array/max-chunks-make-sorted.py
+++ 1-array/max-chunks-make-sorted.py
@@ -1,3 +1,11 @@
+"""
+Leet code 769: Max Chunks To Make Sorted
+Techniques: max so far, prefix sum, monotonic stack
+Complexity: O(n) for all approaches
+Space Complexity: O(1) for max so far and prefix sum, O(n) for monotonic stack
+"""
+
+
 class Solution(object):
     def maxChunksToSorted(self, arr):
         chunks, max_pos = 1, arr[0]
@@ -8,3 +16,22 @@ class Solution(object):
             if max_pos == len(arr) - 1:
                 return chunks
         return chunks
+
+    def maxChunksToSorted(self, arr):
+        curr_sum, exp_sum, chunks = 0, 0, 0
+        for i in range(0, len(arr)):
+            curr_sum += arr[i]
+            exp_sum += i
+            if curr_sum == exp_sum:
+                chunks += 1
+        return chunks
+
+    def maxChunksToSorted(self, arr):
+        s = []
+        for i in range(len(arr)):
+            val = arr[i]
+            while len(s) > 0 and s[-1] > arr[i]:
+                val = max(val, s[-1])
+                s.pop()
+            s.append(val)
+        return len(s)
