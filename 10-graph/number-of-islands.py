877ee1660dd117b21571de6c1d456e1c03f9114d
ycheryl <yulcheryl2357@gmail.com>
2025-08-12 19:22:44 +0700

    count numbers of islands using BFS

---
+++ 10-graph/number-of-islands.py
@@ -0,0 +1,38 @@
+"""
+Leet code 200: Number of Islands
+Techniques: BFS, DFS
+Complexity: O(m * n) where m is the number of rows and n is the number of columns
+Space Complexity: O(m * n) for visited set
+"""
+
+from collections import deque
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        row, col = len(grid), len(grid[0])
+        visited = set()
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+        def traverse(r, c):
+            q = deque([(r, c)])
+            while q:
+                i, j = q.popleft()
+                if (i, j) not in visited:
+                    visited.add((i, j))
+                    for d in directions:
+                        next_r, next_c = i + d[0], j + d[1]
+                        if (
+                            0 <= next_r < row
+                            and 0 <= next_c < col
+                            and grid[next_r][next_c] == "1"
+                        ):
+                            q.append((next_r, next_c))
+
+        count = 0
+        for r in range(row):
+            for c in range(col):
+                if (r, c) not in visited and grid[r][c] == "1":
+                    count += 1
+                    traverse(r, c)
+        return count
