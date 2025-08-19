9b7d4c57fb217a593e9c624bcd01bb236a9cf0ff
ycheryl <yulcheryl2357@gmail.com>
2025-08-19 20:26:22 +0700

    add sorting solution

--- 2-string/group-anagrams.py
+++ 2-string/group-anagrams.py
@@ -1,31 +1,35 @@
 """
-2. Given an array of strings strs, group the anagrams together.
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-Solution: hash table
-Time complexity: O(n * k)
-Space complexity: O(n * k)
-where n is the length of strs and k is the maximum length of a string
-in strs.
+Leet code 49: Group Anagrams
+Techniques: Hash Map, Sorting, Counting
+Complexity: O(n * k log k) where n is the number of strings and k is the maximum length of a string
+Space Complexity: O(n * k) for the hash map
 """
 
 
 class Solution(object):
-    def createKeys(self, string):
-        counter = [""] * 26
-        for char in string:
-            counter[ord(char) - ord("a")] += char
-        return "".join([el for el in counter])
-
+    # sorting
     def groupAnagrams(self, strs):
-        hash_table = {}
-        for string in strs:
-            hash_key = self.createKeys(string)
-            if hash_key in hash_table:
-                hash_table[hash_key].append(string)
+        hash_map = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            if key in hash_map:
+                hash_map[key].append(s)
             else:
-                hash_table[hash_key] = [string]
-        res = []
-        for i in hash_table:
-            res.append(hash_table[i])
-        return res
+                hash_map[key] = [s]
+        return [hash_map[key] for key in hash_map]
+
+    # counting
+    def groupAnagrams(self, strs):
+        if len(strs) == 1:
+            return [strs]
+        hash_map = {}
+        for s in strs:
+            counter = [0] * 26
+            for char in s:
+                counter[ord(char) - 97] += 1
+            key = ".".join(str(num) for num in counter)
+            if key in hash_map:
+                hash_map[key].append(s)
+            else:
+                hash_map[key] = [s]
+        return [hash_map[key] for key in hash_map]
