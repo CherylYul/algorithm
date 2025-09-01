cd9e7c4151b18323f849f3bf70e5b61b21c0d1b3
ycheryl <yulcheryl2357@gmail.com>
2025-09-01 19:12:34 +0700

    use counting

---
+++ codility/most-frequent-character.py
@@ -0,0 +1,38 @@
+"""
+Given string s, return the most frequent character in it.
+If not, return the first character in alphabetical order.
+Constraints: 1 <= |s| <= 100,000
+Techniques: Counting
+Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+
+def solution(s):
+    counter = [0] * 26
+    for char in s:
+        counter[ord(char) - 97] += 1
+    best, idx = 0, 0
+    for i in range(26):
+        if counter[i] > best:
+            best, idx = counter[i], i
+    return chr(idx + 97)
+
+
+test_cases = [
+    {"input": "ddcc", "expected": "c"},
+    {"input": "zyxw", "expected": "w"},
+    {"input": "abcabc", "expected": "a"},
+    {"input": "aabbcc", "expected": "a"},
+    {"input": "zaab", "expected": "a"},
+    {"input": "abc", "expected": "a"},
+    {"input": "a", "expected": "a"},
+    {"input": "ab", "expected": "a"},
+]
+
+for test in test_cases:
+    input_value = test["input"]
+    expected_output = test["expected"]
+    assert solution(input_value) == expected_output
+
+# python3 codility/most-frequent-character.py
